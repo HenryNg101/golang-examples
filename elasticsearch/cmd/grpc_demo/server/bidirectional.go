3e73e2392dde221d92e93f2a2c6e3ec303a81c8f
HenryNg101 <henryng.tech.dev@gmail.com>
2026-04-14 08:58:50 +0000

    Fix error handling for bi-directional, to output better log

--- elasticsearch/cmd/grpc_demo/server/bidirectional.go
+++ elasticsearch/cmd/grpc_demo/server/bidirectional.go
@@ -3,19 +3,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"time"
 
 	pb "github.com/HenryNg101/golang-examples/elasticsearch/grpc-demo/proto"
 	"github.com/HenryNg101/golang-examples/elasticsearch/pkg"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *server) LiveSearch(stream grpc.BidiStreamingServer[pb.SearchRequest, pb.Document]) error {
 	for {
 		req, err := stream.Recv()
 		if err != nil {
-			log.Println("recv error:", err)
+			if err == io.EOF {
+				log.Println("client closed stream")
+				return nil
+			}
+
+			st, ok := status.FromError(err)
+			if ok && st.Code() == codes.Canceled {
+				log.Println("client canceled connection")
+				return nil
+			}
+
+			log.Println("unexpected error:", err)
 			return err
 		}
 		log.Printf("Received query: %s", req.Query)
